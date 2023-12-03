7e53c5cb8d831e204d6bfd839ce1e08910bc70e1
VaibhavBanka <vaibhavbanka12@gmail.com>
2023-12-03 10:38:04 +0530

    Half Home

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/NavBar/Navbar";
 import Contact from "./Pages/Contact";
 import ScrollToTopBtn from "./Components/ScrollToTop/ScrollToTop";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home/>}></Route>
         <Route path="/contact" element={<Contact />} />
       </Routes>
       <ScrollToTopBtn />
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 
 const Home = () => {
-  return <div>Home Page</div>;
-};
+  const [activelink,setactivelink]=useState(0);
+  const [data,setdata]=useState([]);
+  const [loading,setloading]=useState(true);
+  const handleclick=(index)=>{
+    setactivelink(index);
+  }
+
+  const buttons = [
+    "Displine Core",
+    "Discipline Elective",
+    "Discipline-Linked",
+    "Foundation Core-Basic Sc. & Maths",
+    "Foundation Core- Basic Engineering Sc",
+  ];
+
+  const fetchData = async (index) => {
+    try {
+      const response = await fetch(`http://localhost:3000/${buttons[index]}.json`);
+      const jsonData = await response.json();
+      setdata(jsonData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setloading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(activelink);
+  }, [activelink]);
+  return(
+  <div>
+      <ul className="nav nav-pills justify-content-center">
+        {buttons.map((button, index) => (
+          <li key={index} className={`nav-item btn ${activelink === index ? 'active' : ''}`}>
+            <a className={`nav-link ${activelink === index ? 'active' : ''}`} href="#" onClick={() => handleclick(index)}>
+              {button}
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              {Object.keys(data[0] || {}).map((column, i) => (
+                <th key={i}>{column}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((record, i) => (
+              <tr key={i}>
+                {Object.values(record).map((value, j) => (
+                  <td key={j}>{value}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+  </div>
+  );
+}
 
 export default Home;
