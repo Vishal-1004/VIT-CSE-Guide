8a38b42b8dbe30a21e6a90a2e4ad6c89b655681a
VISHAL KUAMR YADAV <vishal100403@gmail.com>
2023-12-13 03:08:25 +0530

    General Subject Schema made

---
+++ server/models/subject.js
@@ -0,0 +1,81 @@
+const mongoose = require("mongoose");
+
+const subjectSchema = new mongoose.Schema({
+  domain: {
+    type: String,
+  },
+  content: [
+    {
+      courseTitle: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      credits: {
+        type: Number,
+        required: true,
+      },
+      syllabus: {
+        type: String,
+      },
+      studyMaterials: [
+        {
+          moduleNo: {
+            type: Number,
+          },
+          moduleName: {
+            type: String,
+          },
+          moduleContent: {
+            type: String,
+          },
+          materialLink: {
+            type: String,
+          },
+        },
+      ],
+      papers: [
+        {
+          examType: {
+            type: String,
+          },
+          examTypeNo: {
+            type: Number,
+            default: 0,
+          },
+          slot: {
+            type: String,
+          },
+          year: {
+            type: String,
+          },
+          paperMaterialLink: {
+            type: String,
+          },
+        },
+      ],
+      referenceVideos: [
+        {
+          moduleNo: {
+            type: Number,
+          },
+          videos: [
+            {
+              topic: {
+                type: String,
+              },
+              videoLink: {
+                type: String,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ],
+});
+
+// creating model
+const subject = new mongoose.model("Subject", subjectSchema);
+
+module.exports = subject;
